5e84d2c5f8ec55d190daa30f4ce3014c1ebac61d
Ken Sodemann <ken@ionic.io>
2019-11-23 08:05:42 -0600

    feat(about): provide about page and versioning

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { version } from '@app/default-data/version';
+import { Version } from '@app/models/version';
 
 @Component({
   selector: 'app-tab3',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['about.page.scss']
 })
 export class AboutPage {
+  appVersion: Version = version;
   constructor() {}
 }
