b1b62ace64042ed455f145615ad7cb673ee44660
Ken Sodemann <ken@ionic.io>
2019-11-23 07:18:48 -0600

    feat(timers): add event emitters

--- src/app/shared/timer-list-item/timer-list-item.component.ts
+++ src/app/shared/timer-list-item/timer-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Timer } from '@app/models/timer';
 
 @Component({
@@ -8,12 +8,13 @@ import { Timer } from '@app/models/timer';
 })
 export class TimerListItemComponent {
   @Input() timer: Timer;
+  @Output() edit: EventEmitter<Timer>;
+  @Output() delete: EventEmitter<Timer>;
+  @Output() toggle: EventEmitter<Timer>;
 
-  buttonClicked() {
-    console.log('Button Clicked');
-  }
-
-  labelClicked() {
-    console.log('Label Clicked');
+  constructor() {
+    this.edit = new EventEmitter();
+    this.delete = new EventEmitter();
+    this.toggle = new EventEmitter();
   }
 }
