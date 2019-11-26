d1aec337773e18f4b41adfa4c0aa6594b64bfd8b
Ken Sodemann <ken@ionic.io>
2019-11-25 21:06:26 -0600

    feat(app): add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -8,17 +11,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { environment } from '@env/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot(reducers, {
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence(),
+    AppRoutingModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    StoreModule.forRoot(reducers, {
       metaReducers,
       runtimeChecks: {
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    })],
+    })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCQp6GiTeW6khMeblDr01vUnnzk7xEgOu0',
+    authDomain: 'personal-task-timer.firebaseapp.com',
+    databaseURL: 'https://personal-task-timer.firebaseio.com',
+    projectId: 'personal-task-timer',
+    storageBucket: 'personal-task-timer.appspot.com',
+    messagingSenderId: '299812045089',
+    appId: '1:299812045089:web:a3fdcd86abf86428a03e60',
+    measurementId: 'G-QKJBL21BSG'
+  }
 };
